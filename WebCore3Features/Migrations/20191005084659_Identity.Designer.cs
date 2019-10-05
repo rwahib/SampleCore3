47716db7a927b0637b66dc2ac858023922506b51
Wahib, Refky <Refky.Wahib@det.nsw.edu.au>
2019-10-05 18:50:59 +1000

    Adding Identity schema

--- WebCore3Features/Migrations/20191005084659_Identity.Designer.cs
+++ WebCore3Features/Migrations/20191005084659_Identity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebCore3Features.Web.DataContext;
 
 namespace WebCore3Features.Web.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20191005084659_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
